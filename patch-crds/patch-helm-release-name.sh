939d4490993cba1c86233d1c1c57ca1edc996008
Tanveer Alam <tan24alam11@gmail.com>
2024-11-02 02:16:28 +0530

    Update patch-helm-release-name.sh

--- patch-crds/patch-helm-release-name.sh
+++ patch-crds/patch-helm-release-name.sh
@@ -11,14 +11,14 @@ for group in "${CRD_GROUPS[@]}"; do
     for crd in $CRDS; do
         echo "Processing CRD: $crd"
 
-        # Check if the release-name annotation is set to "vcontrolplane"
+        # Check if the release-name annotation is set to "eso"
         CURRENT_ANNOTATION=$(kubectl get crd "$crd" -o json | jq -r '.metadata.annotations["meta.helm.sh/release-name"]')
 
-        if [ "$CURRENT_ANNOTATION" == "vcontrolplane" ]; then
+        if [ "$CURRENT_ANNOTATION" == "eso" ]; then
             echo "Updating release-name annotation for CRD $crd"
 
-            # Patch the annotation to change release-name to "vcontrolplane-eso"
-            kubectl patch crd "$crd" --type=json -p='[{"op": "replace", "path": "/metadata/annotations/meta.helm.sh~1release-name", "value": "vcontrolplane-eso"}]'
+            # Patch the annotation to change release-name to "eso"
+            kubectl patch crd "$crd" --type=json -p='[{"op": "replace", "path": "/metadata/annotations/meta.helm.sh~1release-name", "value": "eso"}]'
         else
             echo "No update needed for CRD $crd"
         fi
